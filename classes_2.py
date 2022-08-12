4a0082ef5c252a3904580d65c3802dc5580f90a3
Gary Marc Killen <47016687+killengary37@users.noreply.github.com>
2022-08-11 21:03:50 -0700

    Add files via upload

---
+++ classes_2.py
@@ -0,0 +1,36 @@
+
+from math
+
+class Shape:
+    def area(self)
+        pass
+    
+
+
+class Rectangle(Shape):
+    length = 10
+    width = 10
+
+    def __init__ (self,length,width):
+        self.l = length
+        self.w = width
+
+    def area(self):
+        return self.l * self.w
+
+class Triangle(Shape):
+    base = 12
+    height = 12
+    
+    def __init__ (self,base,height)
+        self.b = base
+        self.h = height
+
+    def area(self)    
+        return .5 * self.b * self.h
+    
+       
+            
+
+
+
